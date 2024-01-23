042ed7198b8e93f30855e9939e5dc90e0cd02cf9
Pavel Zelenko <pasha.zelenko001@gmail.com>
2024-01-23 22:49:08 +0300

    feat: prod v2

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,7 +1,4 @@
 export const BASE_URL = 'https://moblen.ru/api/v1';
+export const BASE_URL_V2 = 'https://moblen.ru/api/v2';
 export const BASE_DEV_URL = 'https://moblen.online/api/v1';
 export const BASE_DEV_URL_V2 = 'https://moblen.online/api/v2';
-
-export const GROUP_REF_LINK = 'https://moblen.ru/registration/ref/';
-
-export const GROUP_DEV_REF_LINK = 'https://moblen.online/registration/ref/';
--- src/services/services.utils.ts
+++ src/services/services.utils.ts
@@ -1,4 +1,4 @@
-import { BASE_DEV_URL, BASE_DEV_URL_V2, BASE_URL } from '../constants/api.ts';
+import { BASE_DEV_URL, BASE_DEV_URL_V2, BASE_URL_V2 } from '../constants/api.ts';
 
 export const DEV_HOSTNAME = 'moblen.online';
 const USE_API_V2 = true;
@@ -10,5 +10,5 @@ const getBaseDevUrl = (): string => {
 export const getBaseUrl = (): string => {
   return location.hostname === DEV_HOSTNAME || location.hostname === 'localhost'
     ? getBaseDevUrl()
-    : BASE_URL;
+    : BASE_URL_V2;
 };
